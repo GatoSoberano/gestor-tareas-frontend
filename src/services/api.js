2a44b1bf5a37ebf6ee68e452571f98de7c0fc2f7
unknown <moinavajas97@gmail.com>
2025-03-16 18:52:18 -0400

    Actualizando API URL para Render

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = 'https://gestor-tareas-backend.onrender.com/api';
 
 export const registerUser = (data) => axios.post(`${API_URL}/auth/register`, data);
 export const loginUser = (data) => axios.post(`${API_URL}/auth/login`, data);
